3e46173b4665fbf1317e3b9d2ab8915662d037a5
royc67 <royc67@gmail.com>
2020-10-06 14:56:31 +0300

    .

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 class Declare{
   //implement here
+  constructor(executor = () => {}){
+    this.handlers =()=>{};
+    this.state = 'pending';
+    this.value = null;
+    executor(this.resolve.bind(this))    
+  }  
+    resolve(value){
+      if (this.state = 'pending'){
+        this.value = value
+        this.state = 'resolved'
+        this.handlers(value)
+      }
+    }
+
+    then(executor = ()=> {}){
+      this.handlers = executor
+      if (this.state === 'resolved') {
+        return new Declare((resolveExecutor) => {
+          resolveExecutor(executor(this.value))
+          }) 
+    } else return this;
+  }
 }
 module.exports = Declare
+
+
